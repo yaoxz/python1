162e1fb63f62849764af41ea384275e02ae31717
yaoxz <yxiangzhou@163.com>
2018-07-19 14:35:03 +0800

    copy file from D:\ to E:"

--- copy.py
+++ copy.py
@@ -11,33 +11,39 @@ Year=str(cur.year).zfill(4)
 #Month=str(cur.month)
 Month=str(cur.month).zfill(2)
 #Day=cur.day
-Day=str(cur.day).zfill(2)
+#Day=str(cur.day).zfill(2)
 print Year,Month
 
-SourcePath="D:\\2018"
-DesPath="E:\\"+Year+Month
-print DesPath
+#SourcePath:H8BAK DIR
+SourcePath="F:\\H8BAK\\YYYYMMDD"
+#Destinatiion path (mobile HDD(mobile hard disk drive)):H
+HDD="H:\\"
+DesPath=HDD+Year+Month
+#print DesPath
+
 if not os.path.exists(DesPath):
-	#print (DesPath,": not exists")
 	os.mkdir(DesPath)
 ###匹配到指定日期的文件后复制
-
 for i in os.listdir(SourcePath):
-	if os.path.isfile(i) and os.path.splitext(i)[1] == '.zip' :
-		#print i
-		#print "star" 
-		#pdb.set_trace()
+	#print os.path.splitext(i)[1]
+	#print os.path.isfile(os.path.join(SourcePath,i))
+	try:
+		if  os.path.splitext(i)[1] == '.bz2' :
+		#print 123
 		#匹配包含指定日期的文件名
-		L=re.compile(r'[a-z]+'+Year+Month+'[0-9a-z]+\.[0-9a-z]+',re.I).match(i)
-		if L:
-			Real_DesPathFile=os.path.join(DesPath,L.group())
-			print Real_DesPathFile
-			#print os.path.abspath(L.group())
-			if not os.path.exists(Real_DesPathFile):
-				Real_SourcePathFile=os.path.join(SourcePath,L.group())
-				#print Real_DesPathFile
-				#print "==="
-				shutil.move(Real_SourcePathFile,DesPath)
-				print Real_SourcePathFile," copy to ",Real_DesPathFile
-		
-		
+			L=re.compile(r'[a-z]+\-'+Year+Month+'[0-9a-z]+\.[0-9a-z]+\.[0-9a-z]+',re.I).match(i)
+			if L:
+				Real_DesPathFile=os.path.join(DesPath,L.group())
+				if not os.path.exists(Real_DesPathFile):
+					Real_SourcePathFile=os.path.join(SourcePath,L.group())
+					shutil.move(Real_SourcePathFile,DesPath)
+					#print Real_SourcePathFile," copy to ",Real_DesPathFile
+				else:
+					if not os.path.exists(os.path.join(HDD,"Duplicate-files")):
+						os.mkdir(os.path.join(HDD,"Duplicate-files"))
+					Real_SourcePathFile=os.path.join(SourcePath,L.group())
+					shutil.move(Real_SourcePathFile,os.path.join(HDD,"Duplicate-files"))
+	except Exception,e:
+		print e
+		print "ERROR,Please Check!...."
+		continue
